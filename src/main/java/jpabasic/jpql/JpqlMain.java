69c38e632283205fd18d3365ff1a2b911ac31a88
jingyu9804 <swc05161@gmail.com>
2023-03-31 00:09:37 +0900

    feat(jpabasic) : JPQL 기본 함수 - locate

--- src/main/java/jpabasic/jpql/JpqlMain.java
+++ src/main/java/jpabasic/jpql/JpqlMain.java
@@ -38,7 +38,71 @@ public class JpqlMain {
         emf.close();
     }
 
-    private static void typePresentTest(EntityManager em) {
+    private static void jpqlBasicFunction(EntityManager em) {
+        String query = "select locate('de','abcdef') from Member m";
+        List<Integer> resultList = em.createQuery(query, Integer.class)
+                .getResultList();
+
+        for (Integer result : resultList) {
+            System.out.println("result = " + result);
+        }
+    }
+
+    private void caseTest2(EntityManager em) {
+        // 사용자 이름이 없으면 이름 없는 회원을 반환한다.
+        String query = "select coalesce(m.username, '이름 없는 회원') from Member m";
+        List<String> resultList = em.createQuery(query, String.class)
+                .getResultList();
+
+        for (String result : resultList) {
+            System.out.println("result = " + result);
+        }
+
+        // 사용자 이름이 관리자면 null 을 반환하고, 나머지는 본인의 이름을 반환한다.
+        String query2 = "select nullif(m.username, '관리자') from Member m";
+        List<String> resultList1 = em.createQuery(query2, String.class)
+                .getResultList();
+
+        for (String result : resultList1) {
+            System.out.println("result = " + result);
+        }
+    }
+
+    private void caseTest1(EntityManager em) {
+        // 기본 case 식
+        String query =
+                "select " +
+                        "case when m.age <= 10 then '학생요금' " +
+                        "     when m.age >= 60 then '경로요금' " +
+                        "     else '일반요금' " +
+                        "end " +
+                "from Member m";
+
+        List<String> resultList = em.createQuery(query, String.class)
+                .getResultList();
+
+        for (String result : resultList) {
+            System.out.println("result = " + result);
+        }
+
+        String query2 =
+                "select " +
+                        "case t.name " +
+                            "when 'teamA' then '성과급 110%!!' " +
+                            "when 'teamB' then '성과급 120%!!' " +
+                            "else '성과급 없음.' " +
+                        "end " +
+                "from Team t";
+
+        List<String> resultList1 = em.createQuery(query2, String.class)
+                .getResultList();
+
+        for (String result : resultList1) {
+            System.out.println("result = " + result);
+        }
+    }
+
+    private void typePresentTest(EntityManager em) {
         // JPQL 타입 표현
         String query = "select m.username, 'HELLO', true from Member m where m.memberType = :userType";
         List<Object[]> resultList = em.createQuery(query)
@@ -48,7 +112,7 @@ public class JpqlMain {
         for (Object[] objects : resultList) {
             System.out.println("objects[0] = " + objects[0]);
             System.out.println("objects[1] = " + objects[1]);
-            System.out.println("objects[2] = " + objects[2]);
+            System.out.println("objects[현2] = " + objects[2]);
         }
 
         // 엔티티 타입 표현 ( 상속 관계에서 사용 )
@@ -61,7 +125,7 @@ public class JpqlMain {
         }
     }
 
-    private static void subQueryTest3(EntityManager em) {
+    private void subQueryTest3(EntityManager em) {
         // 팀 A 소속인 회원. exists (subquery)
         String query1 = "select m from Member m where exists (select t from m.team t where t.name = 'teamA')";
         List<Member> resultList1 = em.createQuery(query1, Member.class)
@@ -104,7 +168,7 @@ public class JpqlMain {
         }
     }
 
-    private static void subQueryTest2(EntityManager em) {
+    private void subQueryTest2(EntityManager em) {
         // 한 건이라도 주문한 고객
         String query = "select m from Member m where (select count(o) from Orders o where m = o.member) > 0";
         List<Member> resultList = em.createQuery(query, Member.class)
@@ -115,7 +179,7 @@ public class JpqlMain {
         }
     }
 
-    private static void subQueryTest1(EntityManager em) {
+    private void subQueryTest1(EntityManager em) {
         // 나이가 평균보다 많은 회원 찾기
         String query = "select m from Member m where m.age > (select avg(m2.age) from Member m2)";
         List<Member> resultList = em.createQuery(query, Member.class)
@@ -126,7 +190,7 @@ public class JpqlMain {
         }
     }
 
-    private static void joinTest(EntityManager em) {
+    private void joinTest(EntityManager em) {
         String query = "select m from Member m right join m.team t on t.name = 'teamA'";
         List<Member> resultList = em.createQuery(query, Member.class)
                 .getResultList();
@@ -136,7 +200,7 @@ public class JpqlMain {
         }
     }
 
-    private static void pagingTest(EntityManager em) {
+    private void pagingTest(EntityManager em) {
         List<Member> resultList = em.createQuery("select m from Member m order by m.age desc", Member.class)
                 .setFirstResult(0)
                 .setMaxResults(10)
@@ -148,7 +212,7 @@ public class JpqlMain {
         }
     }
 
-    private static void forPagingTestInit(EntityManager em) {
+    private void forPagingTestInit(EntityManager em) {
         for (int i = 0; i < 100; i++) {
             Member member = new Member();
             member.setUsername("member" + i);
@@ -157,7 +221,7 @@ public class JpqlMain {
         }
     }
 
-    private static void scalaTypeProjection2(EntityManager em) {
+    private void scalaTypeProjection2(EntityManager em) {
         List<MemberDTO> resultList = em.createQuery("select new jpabasic.jpql.domain.MemberDTO(m.username, m.age) from Member m", MemberDTO.class)
                 .getResultList();
 
@@ -172,7 +236,7 @@ public class JpqlMain {
         em.clear();
     }
 
-    private static void scalaTypeProjection(EntityManager em) {
+    private void scalaTypeProjection(EntityManager em) {
         List resultList = em.createQuery("select m.username, m.age from Member m")
                 .getResultList();
 
@@ -189,7 +253,7 @@ public class JpqlMain {
         em.persist(teamA);
 
         Member member1 = new Member();
-        member1.setUsername("member1");
+        member1.setUsername("   관리자   ");
         member1.setAge(20);
         member1.changeTeam(teamA);
         em.persist(member1);
@@ -219,7 +283,7 @@ public class JpqlMain {
         em.persist(orders);
     }
 
-    private static void nativeQueryTest1(EntityManager em) {
+    private void nativeQueryTest1(EntityManager em) {
         // Query 가 실행되기 전에 flush 호출
         // Native SQL
         String sql = "select * from member where name = 'member1'";
@@ -230,7 +294,7 @@ public class JpqlMain {
         }
     }
 
-    private static void criteriaTest1(EntityManager em) {
+    private void criteriaTest1(EntityManager em) {
         // Criteria 사용 준비
         CriteriaBuilder cb = em.getCriteriaBuilder();
         CriteriaQuery<Member> query = cb.createQuery(Member.class);
@@ -247,7 +311,7 @@ public class JpqlMain {
         }
     }
 
-    private static void jpqlTest1(EntityManager em) {
+    private void jpqlTest1(EntityManager em) {
         // JPQL 검색
         String jpql = "select m From Member m where m.age > 18";
         List<Member> result = em.createQuery(jpql, Member.class)

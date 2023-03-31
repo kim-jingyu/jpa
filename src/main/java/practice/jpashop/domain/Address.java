4b894c775b4acebb9e59a72a4866cf423836a5a6
jingyu9804 <swc05161@gmail.com>
2023-04-01 04:25:54 +0900

    feat(jpashop) : Address 값 타입 immutable 하게 설계

--- src/main/java/practice/jpashop/domain/Address.java
+++ src/main/java/practice/jpashop/domain/Address.java
@@ -4,14 +4,22 @@ import jakarta.persistence.Embeddable;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * 값 타입은 immutable 하게 설계한다.
+ */
 @Embeddable
 @Getter
-@Setter
 public class Address {
     private String city;
     private String street;
     private String zipcode;
 
     public Address() {
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
     }
 }

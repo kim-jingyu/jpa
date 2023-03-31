626a41e07150ac9e3c3979e2aed0f048bb8b9921
jingyu9804 <swc05161@gmail.com>
2023-04-01 02:43:23 +0900

    feat(jpashop) : Address 값 타입 구현

---
+++ src/main/java/practice/jpashop/domain/Address.java
@@ -0,0 +1,17 @@
+package practice.jpashop.domain;
+
+import jakarta.persistence.Embeddable;
+import lombok.Getter;
+import lombok.Setter;
+
+@Embeddable
+@Getter
+@Setter
+public class Address {
+    private String city;
+    private String street;
+    private String zipcode;
+
+    public Address() {
+    }
+}

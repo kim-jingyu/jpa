b48fb831816c386b98bd8bcea19f9c243a8aa39f
jingyu9804 <swc05161@gmail.com>
2023-04-01 02:42:04 +0900

    feat(jpashop) : Member Entity 구현

--- src/main/java/practice/jpashop/domain/Member.java
+++ src/main/java/practice/jpashop/domain/Member.java
@@ -1,10 +1,10 @@
 package practice.jpashop.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 @Entity
 @Getter @Setter
@@ -12,9 +12,17 @@ public class Member {
 
     @Id
     @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
 
     private String username;
 
+    @Embedded
+    private Address address;
 
+    @OneToMany(mappedBy = "member")
+    private List<Orders> orders;
+
+    public Member() {
+    }
 }

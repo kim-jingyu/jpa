80ff0c720d3c199142cc36a004f94a9d8977b375
jingyu9804 <swc05161@gmail.com>
2023-04-01 02:48:39 +0900

    feat(jpashop) : Orders(주문) 엔티티 구현

---
+++ src/main/java/practice/jpashop/domain/Orders.java
@@ -0,0 +1,31 @@
+package practice.jpashop.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Orders {
+    @Id @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @OneToOne
+    @JoinColumn(name = "delivery_id")
+    private Delivery delivery;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems;
+
+    private LocalDateTime orderDate;
+    private OrderStatus status;
+}

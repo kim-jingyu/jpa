55d7856b3f2ac0867cf56cc0beb3e753210a3d26
jingyu9804 <swc05161@gmail.com>
2023-04-01 03:05:27 +0900

    feat(jpashop) : OrderItem 엔티티 구현

---
+++ src/main/java/practice/jpashop/domain/OrderItem.java
@@ -0,0 +1,27 @@
+package practice.jpashop.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class OrderItem {
+    @Id @GeneratedValue
+    @Column(name = "order_item_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private Orders order;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    private int orderPrice;     // 주문 금액
+    private int count;          // 주문 수량
+
+    public OrderItem() {
+    }
+}

14640dc618393302071fd80774672b95cdf82c8c
jingyu9804 <swc05161@gmail.com>
2023-04-02 03:19:23 +0900

    feat(jpashop) : 주문상품(OrderItem) 엔티티 구현

--- src/main/java/practice/jpashop/domain/OrderItem.java
+++ src/main/java/practice/jpashop/domain/OrderItem.java
@@ -24,4 +24,43 @@ public class OrderItem {
 
     public OrderItem() {
     }
+
+    // 생성 메서드
+
+    /**
+     * 주문 상품, 가격, 수량 정보를 사용해서 주문상품 엔티티를 생성한다.
+     * @param item
+     * @param orderPrice
+     * @param count
+     * @return 주문상품 엔티티
+     */
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    // 비즈니스 로직
+
+    /**
+     * 주문 취소
+     * 취소한 주문 수량만큼 상품의 재고를 증가시킨다.
+     */
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    // 조회 로직
+
+    /**
+     * 주문상품 전체 가격 조회
+     * @return 주문 가격에 수량을 곱한 값
+     */
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }

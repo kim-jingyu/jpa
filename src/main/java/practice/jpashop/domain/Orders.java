070861d6ec937638d53a8c446680ad10c2884c97
jingyu9804 <swc05161@gmail.com>
2023-04-01 22:42:11 +0900

    feat(jpashop) : 연관관계 편의 메서드 생성

--- src/main/java/practice/jpashop/domain/Orders.java
+++ src/main/java/practice/jpashop/domain/Orders.java
@@ -31,6 +31,22 @@ public class Orders {
     @Enumerated(value = EnumType.STRING)
     private OrderStatus status;
 
+    // 연관관계 편의 메서드
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
+
     public Orders() {
     }
 }

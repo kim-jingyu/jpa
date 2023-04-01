e417dfeb506a3764fe42491b72e2fe68fcd9410b
jingyu9804 <swc05161@gmail.com>
2023-04-02 01:19:28 +0900

    feat(jpashop) : 상품 엔티티 구현 ( 비즈니스 로직 추가 )

--- src/main/java/practice/jpashop/domain/Item.java
+++ src/main/java/practice/jpashop/domain/Item.java
@@ -3,6 +3,7 @@ package practice.jpashop.domain;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import practice.jpashop.exception.NotEnoughStockException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,4 +23,26 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    // 비즈니스 로직 추가
+
+    /**
+     * 재고수량 증가
+     * @param quantity
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * 재고수량 감소
+     * @param quantity
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }

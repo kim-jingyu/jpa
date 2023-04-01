5e642a9a0a3022cefd305246349cab73eee0808c
jingyu9804 <swc05161@gmail.com>
2023-04-02 03:19:11 +0900

    feat(jpashop) : 주문(Orders) 엔티티 구현

--- src/main/java/practice/jpashop/domain/Orders.java
+++ src/main/java/practice/jpashop/domain/Orders.java
@@ -50,4 +50,61 @@ public class Orders {
 
     public Orders() {
     }
+
+    // 생성 메서드
+
+    /**
+     * 주문 엔티티를 생성할 때 사용한다.
+     * 주문 회원, 배송정보, 주문상품의 정보를 받아서 실제 주문 엔티티를 생성한다.
+     * @param member
+     * @param delivery
+     * @param orderItems
+     * @return 실제 주문 엔티티
+     */
+    public static Orders createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Orders order = new Orders();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    // 비즈니스 로직
+
+    /**
+     * 주문 취소
+     * 주문 상태를 취소로 변경하고, 주문상품에 주문 취소를 알린다.
+     * 만약 이미 배송을 완료한 상품이면 주문을 취소하지 못하도록 예외를 발생시킨다.
+     */
+    public void cancel() {
+        if (delivery.getStatus() == DeliveryStatus.COMPLETE) {
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+
+        this.setStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    // 조회 로직
+
+    /**
+     * 주문 시 사용한 전체 주문 가격 조회
+     * 전체 주문 가격을 알려면 각각의 주문상품 가격을 알아야 한다.
+     * 따라서 연관된 주문상품들의 가격을 조회해서 더한 값을 반환한다.
+     * 이때 실무에서는 주로 주문에 전체 주문 가격 필드를 두고 역정규화 한다.
+     * @return 전체 주문 가격
+     */
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        for (OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
+    }
 }

57ef0de5b4b17705e271fd736a80e1b56df7af0c
jingyu9804 <swc05161@gmail.com>
2023-04-05 18:52:18 +0900

    chore(jpashop) : insert sql 주석처리

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,2 @@
-insert into member(member_id,username, city, street, zipcode) values(1L, '김진규', '서울', '강남', '11111');
-insert into item(dtype, item_id, name, price, stock_quantity) values('Book', 1L, '정석', 8000, 1000);
+--insert into member(member_id,username, city, street, zipcode) values(1L, '김진규', '서울', '강남', '11111');
+--insert into item(dtype, item_id, name, price, stock_quantity) values('Book', 1L, '정석', 8000, 1000);

83ab79b398a3f09eb82403e61fa35a193391e9bc
jingyu9804 <swc05161@gmail.com>
2023-04-10 18:20:51 +0900

    feat(datajpa) : 스프링 데이터 JPA 페이징과 정렬

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,5 +1,9 @@
 package study.datajpa.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -51,4 +55,20 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
      */
     @Query("select m from Member m where m.username in :names")
     List<Member> findByNames(@Param("names") List<String> names);
+
+    // 페이징과 정렬 사용
+    Page<Member> findPageByUsername(String name, Pageable pageable); // Page -> count 쿼리 사용
+    Slice<Member> findSliceByAge(int age, Pageable pageable); // Slice -> count 쿼리 사용 안함
+    List<Member> findListByAge(int age, Pageable pageable); // List -> count 쿼리 사용 안함
+    List<Member> findSortByUsername(String name, Sort sort); // Sort -> 정렬 기능
+    Page<Member> findPageByAge(int age, Pageable pageable);
+
+    // count 쿼리 분리
+    @Query(value = "select m from Member m left join m.team t", countQuery = "select count(m) from Member m")
+    Page<Member> findMemberAllCountByAge(int age, Pageable pageable);
+
+    // Top, First
+    List<Member> findTop2By();
+
+    List<Member> findFirst3By();
 }

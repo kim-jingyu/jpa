98a14894d82b456231902c61ed757bc26d18613a
jingyu9804 <swc05161@gmail.com>
2023-04-19 19:39:44 +0900

    test(querydsl) : 동적 쿼리 - Builder 사용

--- src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
@@ -7,7 +7,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.Team;
 
 import java.util.List;
 
@@ -62,5 +65,33 @@ class MemberJpaRepositoryTest {
         // 회원 이름으로 찾기 테스트
         List<Member> foundMemberByName = memberJpaRepository.findByUsername_querydsl("user1");
         assertThat(foundMemberByName).containsExactly(user1);
+    }
+
+    @Test
+    @DisplayName("동적 쿼리 - Builder 사용")
+    void 동적쿼리_Builder() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member user1 = new Member("user1", 10, teamA);
+        Member user2 = new Member("user2", 20, teamA);
+        Member user3 = new Member("user3", 30, teamB);
+        Member user4 = new Member("user4", 40, teamB);
+        em.persist(user1);
+        em.persist(user2);
+        em.persist(user3);
+        em.persist(user4);
+
+        MemberSearchCondition condition = new MemberSearchCondition();
+        condition.setAgeGoe(15);
+        condition.setAgeLoe(40);
+        condition.setTeamName("teamA");
+
+        List<MemberTeamDto> result = memberJpaRepository.searchByBuilder(condition);
+
+        assertThat(result).extracting("username").containsExactly("user2");
+
     }
 }
